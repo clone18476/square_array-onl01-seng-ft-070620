53a85f8b7e9a9a5069ad6bb4c941dca57c05a257
Cameron Cousins <cameroncousins18@gmail.com>
2020-07-27 01:24:07 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,6 @@
 def square_array(array)    #define method with argument of array 
   arr = []     #set arr to value of [] (an empty array) 
-  array.each { |i| arr << i ** 2 }      #use each to square 
+  array.each { |i| arr << i ** 2 }      #use .each to square the value of `array`, and then assign the squared value to an array called `arr`
   arr
 end
 
